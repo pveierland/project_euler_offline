05b866a1cedb1a5722468dc544a0c27a4234789c
Per Magnus Veierland <pveierland@gmail.com>
2026-03-15 17:46:51 +0800

    feat: Simplify development environment

--- flake.nix
+++ flake.nix
@@ -10,43 +10,42 @@
   outputs = inputs@{ self, flake-utils, nixpkgs, nixpkgs-unstable, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        nixpkgsSystem = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; };
+        pkgs-unstable = import nixpkgs-unstable { inherit system; };
 
-        nixpkgsSystemUnstable = (import nixpkgs-unstable {
-          inherit system;
-        });
+        pandoc-python = pkgs.python312Packages.buildPythonPackage rec {
+          pname = "pandoc";
+          version = "2.4";
+          src = pkgs.python312Packages.fetchPypi {
+            inherit pname version;
+            hash = "sha256-7NH4y7f0GAxrXbShenwadN9RmZX18Ybvgc5yqcvQ3Zo=";
+          };
+          propagatedBuildInputs = with pkgs.python312Packages; [
+            plumbum
+            ply
+          ];
+          doCheck = false;
+        };
 
-        lib = nixpkgsSystem.lib;
-        pkgs = nixpkgsSystem.pkgs;
-        pkgs-unstable = nixpkgsSystemUnstable.pkgs;
-
-        pythonEnvironment = pkgs.python312.withPackages (p: (
-          lib.unique (lib.flatten (map (source: import source { pythonPackages = p; }) pythonEnvironmentSources))
-        ));
-
-        pythonEnvironmentSources = [
-          ./python-requirements.nix
-        ];
+        pythonEnvironment = pkgs.python312.withPackages (p: [
+          p.aiohttp
+          p.beautifulsoup4
+          p.pydash
+          p.tqdm
+          pandoc-python
+        ]);
       in
       {
-        devShells.default = (
-          pkgs.mkShell
-            {
-              nativeBuildInputs = (
-                with pkgs; [
-                  okular
-                  pandoc
-                  pkgs-unstable.ruff
-                  pkgs-unstable.uv
-                  pythonEnvironment
-                  texlive.combined.scheme-full
-                ]
-              );
-              shellHook = ''
-                UV_PYTHON_ENV_SITE_PACKAGES="${pythonEnvironment}/${pythonEnvironment.sitePackages}"
-              '';
-            }
-        );
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            okular
+            pandoc
+            pkgs-unstable.ruff
+            pkgs-unstable.uv
+            pythonEnvironment
+            texlive.combined.scheme-full
+          ];
+        };
       }
     );
 }
--- python-requirements.nix
+++
@@ -1,6 +0,0 @@
-{ pythonPackages }:
-[
-  pythonPackages.beautifulsoup4
-  pythonPackages.pip
-  pythonPackages.pip-tools
-]
